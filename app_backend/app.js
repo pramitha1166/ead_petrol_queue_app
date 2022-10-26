c58e5cd57ec612948bcba8612bb517ed7ff5d11a
shihara <shihara@surge.global>
2022-10-26 20:13:27 +0530

    back end

--- app_backend/app.js
+++ app_backend/app.js
@@ -1,14 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require('./src/api/user_routes')
-const morgan = require('morgan')
+"use strict";
 
-const app = express()
+const express = require("express");
+const env = require("dotenv");
+const cors = require("cors");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use('/api/user', userRoutes)
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
 
-app.listen(3000, () => {
-    console.log("Server stated on " + 3000)
-})
+//db connection
+const DBConnection = require("./src/repository/DBConnection");
+//routes
+const productRoute = require("./src/controller/productController");
+
+const PORT = process.env.PORT || 5002;
+
+//enable environment varbiables file
+env.config();
+
+//app middlewares
+app.use(express.json());
+app.use(cors());
+
+//create DB connection
+DBConnection(io);
+
+//route middlewares
+app.use("/api/products", productRoute);
+
+io.on("connection", () => {
+  console.log("A new connection has been established");
+});
+server.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
